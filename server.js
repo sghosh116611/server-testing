1d98721a46dd871c79becb055469d1d78a841746
Soumojit Ghosh <ghoshsoumojit53@gmail.com>
2020-03-19 19:16:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const hbs = require("hbs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+hbs.registerHelper("currentYear", () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("uppertext", (text) => {
+    return text.toUpperCase();
+});
+app.get("/", (req, res) => {
+    res.render("home.hbs", {
+        welcomeMessage: "Hi! This is your home page.",
+        pageTitle: "welcome to your home page!",
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        welcomeMessage: "Hi! This is your about page.",
+        currentYear: new Date().getFullYear()
+    });
+});
+
+
+console.log("Starting server!");
+app.listen(3000);
